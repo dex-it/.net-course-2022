d895b55eeaa1d8f0d194ed1973c9f6951045992f
StarlinkG <donikovv228@yandex.ru>
2022-08-23 14:11:21 +0300

    practice

---
+++ Models/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace Models
+{
+    public class Person
+    {
+        public int id_pas { get; set; }
+        public string f_name { get; set; }
+        public string l_name { get; set; }
+    }
+
+    public class Emploeyy : Person
+    {
+        public int id { get; set; }
+        public string contract { get; set; }
+
+        public Emploeyy(int id, string contract)
+        {
+            this.id = id;
+            this.contract = contract;
+        }
+    }
+
+    public class Client
+    {
+
+    }
+
+    public struct Currency
+    {
+        public int CurName { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ PracticeWithTypes/Program.cs
@@ -0,0 +1,5 @@
+﻿using Models;
+using System.Diagnostics.Contracts;
+
+var Emp = new Emploeyy(234234234,"ELfd");
+Console.WriteLine(Emp);
