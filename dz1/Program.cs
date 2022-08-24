9200767c20f74d286badac10fc347aa6ee7e5479
vyusalb@inbox.ru <vyusalb@inbox.ru>
2022-08-24 20:09:19 +0300

    task1

---
+++ dz1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Models;
+
+namespace PracticeWithTypes
+{
+    class Programm
+    {
+        static void Main()
+        {
+            Employee employee = new Employee("Виталий",19);
+            ChangeCurrency(employee);
+            ChangeContract(employee);
+            employee.ShowEmployee();
+        }
+        public static void ChangeContract(Employee employee)
+        {
+            employee.ChangeContracts();
+        }
+        public static void ChangeCurrency(Employee employee)
+        {
+            Currency cur = new Currency(EnumCur.EUR, 400);
+            employee.ChangeCurrency(cur);
+        }
+    }
+}
+
