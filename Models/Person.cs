d68b94740d19d023c52f18cc611d96d778964e3e
Erhan <mabruka87@gmail.com>
2022-08-24 20:49:09 +0300

    добавить файлы

---
+++ Models/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace Models
+{
+    public class Person
+    {
+        public string? name;
+        public int? age;
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void Print() => Console.WriteLine($"Name: {name} - {age}");
+    }
+    public class Employee : Person
+    {
+        public string contract;
+        public Employee(string name, int age) : base(name, age)
+        { }
+        public Employee(string name, int age, string contract) : base(name, age)
+        {
+            this.contract = contract;
+        }
+    }
+
+    public class Client : Person
+    {
+        public Client(string name, int age) : base(name, age)
+        {
+        }
+    }
+    public struct Currency
+    {
+        public string currency;
+        public double price;
+    }
+}
---
+++ PracticeWithTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿using Models;
+using System;
+
+public class PracticeWithTypes
+{
+    static void Main()
+    {
+        Person tom = new Person("Tom", 25);
+        tom.Print();
+
+        Employee emp = new Employee("Bob", 32);
+        ChangeContract(emp);
+
+        Currency curs = new Currency();
+        curs.currency = "Rub";
+        curs.price = 0.22;
+        Console.WriteLine($"Валюта {curs.currency} курс {curs.price}");
+        ChangeCur(curs);
+    }
+
+    /* а) метод, обновляющий контракт сотрудника. Принимает на вход сотрудника и создает контракт (свойство класса “Employee”, строка) наоснове его данных. 
+  Результат присваивается обратно в тело сотрудника, свойству “Contract”; 
+    ? */
+    static void ChangeContract(Employee emp)
+    {
+        emp.contract = "Контракт";
+        Console.WriteLine($"Имя {emp.name} возраст {emp.age} контракт {emp.contract}");
+    }
+
+    /* б) метод обновляющий валюты. (Метод принимает на вход экземпляр структуры “Currency”, меняет значение ее свойств);
+    Сравнить результат работы методов и объяснить его.  */
+    static void ChangeCur(Currency curs_p)
+    {
+        curs_p.currency = "USD";
+        curs_p.price = 16.3;
+        Console.WriteLine($"Валюта {curs_p.currency} курс {curs_p.price}");
+    }
+}
