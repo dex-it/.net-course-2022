1697dd8ab3f06dab12d6d3e890b30829b1b3b694
arinvolosh <arinko8150@gmail.com>
2022-08-24 12:04:29 +0300

    initial commit

---
+++ Models/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Currency
+    {
+        public Currency(string nameCurrency)
+        {
+            NameCurrency = nameCurrency;
+        }
+
+        public string NameCurrency { get; set; }
+    }
+}
