065f29b7199d5a2a8ea6a5ad52fae11c4bd3fd57
arinvolosh <arinko8150@gmail.com>
2022-08-27 21:52:38 +0300

    1 change

---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Models
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public DateTime Data { get; set; }
+        public int NumPasport { get; set; }
+        public Person( string name, DateTime data, int numPasport)
+        {
+            this.name = name;
+            Data = data;
+            NumPasport = numPasport;
+        }
+    }
+}
