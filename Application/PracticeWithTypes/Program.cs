c5ecd0a5785fde9ce51b5e3545bbacc4d503f955
HomerOff <homeroff76@gmail.com>
2022-08-28 14:50:18 +0300

    first classwork fix: changed methods and objects

---
+++ Application/PracticeWithTypes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Models;
+
+namespace PracticeWithTypes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employee = new Employee{
+            FirstName = "Иван",
+            LastName = "Иванов",
+            BirthdayDate = DateTime.Parse("23.01.2000"),
+            PhoneNumber = 077700077,
+            
+        };
+        UpdateContractEmployeeIncorrect(employee);
+        employee.Contract = UpdateContractEmployeeCorrect(employee.FirstName, employee.LastName, employee.BirthdayDate, employee.PhoneNumber);
+
+        Currency currency = new Currency
+        {
+            Code = 978,
+            Name = "euro"
+        };
+        int newCurrencyCode = 840;
+        string newCurrencyName = "dollar";
+        UpdateCurrencyIncorrect(currency, newCurrencyCode, newCurrencyName);
+        currency = UpdateCurrencyCorrect(currency, newCurrencyCode, newCurrencyName);
+    }
+    static void UpdateContractEmployeeIncorrect(Employee employee)
+    {
+       employee.Contract = $"Работник {employee.FirstName} {employee.LastName} принимается на работу в банк по профессии (должности) \"Банкир\"\n" +
+                           $"Дата рождения сотрудника: {employee.BirthdayDate.ToString("dd/MM/yyyy")}\n" +
+                           $"Контактный номер: {employee.PhoneNumber}\n";
+    }
+
+    static string UpdateContractEmployeeCorrect(string firstNameText, string lastNameText, DateTime birthdayDateText, int phoneNumberText)
+    {
+        return $"Работник {firstNameText} {lastNameText} принимается на работу в банк по профессии (должности) \"Банкир\"\n" + 
+               $"Дата рождения сотрудника: {birthdayDateText.ToString("dd/MM/yyyy")}\n" + 
+               $"Контактный номер: {phoneNumberText}\n";
+    }
+    
+    static void UpdateCurrencyIncorrect(Currency currency, int code, string name)
+    {
+        currency.Code = code;
+        currency.Name = name;
+    }
+    
+    static Currency UpdateCurrencyCorrect(Currency currency, int code, string name)
+    {
+        currency.Code = code;
+        currency.Name = name;
+        
+        return currency;
+    }
+}
